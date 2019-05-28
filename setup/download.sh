d4ac9e02d26434474fc4cb8f0c2a3ebadca328e8
Respy <LukeS@omega.no>
2019-05-28 13:45:25 +0800

    fixes

---
+++ setup/download.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Make sure we're not running at root to start
+if [ $(id -u) -eq 0 ]; then
+  echo "This should not be run with sudo. It will be called for the"
+  echo "commands that require it. Try again without sudo."
+else
+
+sudo ./uninstall.sh
+curl -s "https://raw.githubusercontent.com/mRaRn/GaragePi-simple/master/setup/online_install.sh" | bash
